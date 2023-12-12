203c63a7afd44807122c889691936962abbd0ad0
Eric Silk <eric.silk@ericsilk.com>
2023-12-12 12:42:29 -0600

    stuck on problem 1 yayyyy

--- ece530/ece530/hw9/arnoldi.py
+++ ece530/ece530/hw9/arnoldi.py
@@ -1,4 +1,4 @@
-import sys
+#!/usr/bin/env python3
 import numpy as np
 
 float_formatter = "{:.2f}".format
@@ -6,53 +6,53 @@ float_formatter = "{:.2f}".format
 np.set_printoptions(formatter={"float_kind": float_formatter})
 
 
-# Taken from: https://en.wikipedia.org/wiki/Arnoldi_iteration#The_Arnoldi_iteration
-def arnoldi_iteration(A, b, n: int):
-    """Compute a basis of the (n + 1)-Krylov subspace of the matrix A.
+def is_upper_hessenberg(A: np.ndarray) -> bool:
+    return np.isclose(np.tril(A, k=-2), 0.0, atol=1e-3).all()
 
-    This is the space spanned by the vectors {b, Ab, ..., A^n b}.
 
-    Parameters
-    ----------
-    A : array_like
-        An m x m array.
-    b : array_like
-        Initial vector (length m).
-    n : int
-        One less than the dimension of the Krylov subspace, or equivalently the *degree* of the Krylov space. Must be >= 1.
+def is_lower_hessenberg(A: np.ndarray) -> bool:
+    return np.isclose(np.triu(A, k=2), 0.0, atol=1e-3).all()
 
-    Returns
-    -------
-    Q : numpy.array
-        An m x (n + 1) array, where the columns are an orthonormal basis of the Krylov subspace.
-    h : numpy.array
-        An (n + 1) x n array. A on basis Q. It is upper Hessenberg.
-    """
-    eps = 1e-12
-    h = np.zeros((n + 1, n))
-    Q = np.zeros((A.shape[0], n + 1))
+
+# Adapted from: https://relate.cs.illinois.edu/course/cs450-s18/file-version/587fb0505883e0622b9b144b532b5e9a4e7a3684/demos/upload/04-eigenvalues/Arnoldi%20iteration.html
+def arnoldi_iteration(A, b):
+    n = A.shape[0]
+    H = np.zeros((n, n))
+    Q = np.zeros((n, n))
     # Normalize the input vector
-    Q[:, 0] = b / np.linalg.norm(b, 2)  # Use it as the first Krylov vector
-    for k in range(1, n + 1):
-        v = np.dot(A, Q[:, k - 1])  # Generate a new candidate vector
-        for j in range(k):  # Subtract the projections on previous vectors
-            h[j, k - 1] = np.dot(Q[:, j].conj(), v)
-            v = v - h[j, k - 1] * Q[:, j]
-        h[k, k - 1] = np.linalg.norm(v, 2)
-        if h[k, k - 1] > eps:  # Add the produced vector to the list, unless
-            Q[:, k] = v / h[k, k - 1]
-        else:  # If that happens, stop iterating.
-            return Q, h
-    return Q, h
+    Q[:, 0] = b / np.linalg.norm(b)  # Use it as the first Krylov vector
+    for k in range(n):
+        u = A @ Q[:, k]
+        for j in range(k + 1):
+            qj = Q[:, j]
+            H[j, k] = qj @ u
+            u -= H[j, k] * qj
+        if k + 1 < n:
+            H[k + 1, k] = np.linalg.norm(u)
+            Q[:, k + 1] = u / H[k + 1, k]
+
+    return Q, H
 
 
 if __name__ == "__main__":
     A = np.array([[1, 2, 3], [2, 2, 4], [3, 4, 4]])
     b = np.array([1, -1, 1])
 
-    q, h = arnoldi_iteration(A, b, 3)
+    q, h = arnoldi_iteration(A, b)
+    print("Did arnoldi work:")
+    print(np.isclose((q.T @ A @ q - h) / np.linalg.norm(A), 0.0, atol=1e-3).all())
 
     print("Q:")
     print(q)
     print("H:")
     print(h)
+    uh = is_upper_hessenberg(h)
+    lh = is_lower_hessenberg(h)
+    if uh:
+        print("Wow its upper Hessenberg, whoda thunk it")
+    if lh:
+        print("Wow its lower Hessenberg, whoda thunk it")
+    if lh and uh:
+        print(
+            "Wow it's tridiagonal, its cuz the matrix A is Hermitian (or Symmetric cuz its real)"
+        )
